00fd4f6a8e1e91e52a5fcef21aab5e8c66ac8140
ljy0221 <jaeyeong221@gmail.com>
2024-03-16 15:08:16 +0900

    로그아웃 임시구현

--- capstones/lib/screens/setting.dart
+++ capstones/lib/screens/setting.dart
@@ -1,10 +1,40 @@
+import 'package:capstones/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class Setting extends StatelessWidget {
+class Setting extends StatefulWidget {
   const Setting({super.key});
 
   @override
+  State<Setting> createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  static const storage = FlutterSecureStorage();
+
+  logout() async {
+    await storage.delete(key: 'memberId');
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const MyApp(),
+        ));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('setting'),
+        centerTitle: true,
+        elevation: 2,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: logout,
+          child: const Text('로그아웃'),
+        ),
+      ),
+    );
   }
 }
